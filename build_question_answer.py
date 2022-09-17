962a7be7759baf1225669d6074cfa55db808325a
JennyShen056 <112578566+JennyShen056@users.noreply.github.com>
2022-09-17 20:53:26 +0000

    basic structure of python format

--- build_question_answer.py
+++ build_question_answer.py
@@ -4,13 +4,13 @@ import openai
 def generate_question_answer(prompt):
     openai.api_key = "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     response = openai.Completion.create(
-    engine="davinci",
-    prompt=prompt,
-    temperature=0.9,
-    max_tokens=150,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=["\n", " Human:", " AI:"]
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=["\n", " Human:", " AI:"]
     )
     return response.choices[0].text
