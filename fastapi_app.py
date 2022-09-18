318e336acf91372f975548ac81cc2a1542a731c4
JennyShen056 <112578566+JennyShen056@users.noreply.github.com>
2022-09-18 17:14:57 +0000

    adding microservice

--- fastapi_app.py
+++ fastapi_app.py
@@ -6,14 +6,14 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to ETH land! Please start search the latest ETH transactions!"}
+    return {"message": "Welcome to stock return analysis tool! Please start by choosing a stock ticker lol"}
 
 @app.get("/query")
 async def query():
-    """Execute a SQL query from Walmart database to search the latest ETH transactions that includes holidays and non-hoildays"""
+    """Execute a SQL query from the Stocks database to find out the average daily return of a specific stock"""
 
     result = querydb()
-    return {"Latest ETH transactions information": result}
+    return {"Average daily return of the ": result}
 
 
 if __name__ == '__main__':
